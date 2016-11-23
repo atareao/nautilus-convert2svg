d822ca6457efbe6d30443c42b7f1836cc2040882
lorenzo-carbonell <lorenzo.carbonell.cerezo@gmail.com>
2016-11-23 20:35:31 +0100

    IdleObject

--- src/nautilus-convert2svg.py
+++ src/nautilus-convert2svg.py
@@ -21,14 +21,19 @@
 #
 #
 #
+import gi
+try:
+    gi.require_version('Gtk', '3.0')
+    gi.require_version('Nautilus', '3.0')
+except Exception as e:
+    print(e)
+    exit(-1)
 import os
 import subprocess
 import shlex
-import threading
 import tempfile
 import shutil
-from PIL import Image
-from Queue import Queue
+from threading import Thread
 from urllib import unquote_plus
 from gi.repository import GObject
 from gi.repository import Gtk
@@ -38,29 +43,184 @@ from gi.repository import Nautilus as FileManager
 EXTENSIONS_FROM = ['.bmp', '.eps', '.gif', '.jpg', '.pcx', '.png', '.ppm',
                    '.tif', '.tiff', '.webp']
 SEPARATOR = u'\u2015' * 10
-NUM_THREADS = 4
 
 _ = str
 
 
-def increase(worker, image, progreso):
-    GLib.idle_add(progreso.increase)
-    return False
+class IdleObject(GObject.GObject):
+    """
+    Override GObject.GObject to always emit signals in the main thread
+    by emmitting on an idle handler
+    """
+    def __init__(self):
+        GObject.GObject.__init__(self)
+
+    def emit(self, *args):
+        GLib.idle_add(GObject.GObject.emit, self, *args)
 
 
-def ejecuta(comando):
-    print(comando)
-    args = shlex.split(comando)
-    p = subprocess.Popen(args, bufsize=10000, stdout=subprocess.PIPE)
-    valor = p.communicate()[0]
-    return valor
+class DoItInBackground(IdleObject, Thread):
+    __gsignals__ = {
+        'started': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (int,)),
+        'ended': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (bool,)),
+        'start_one': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (str,)),
+        'end_one': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (float,)),
+    }
+
+    def __init__(self, elements):
+        IdleObject.__init__(self)
+        Thread.__init__(self)
+        self.elements = elements
+        self.stopit = False
+        self.ok = True
+        self.daemon = True
+        self.process = None
+
+    def stop(self, *args):
+        self.stopit = True
+
+    def convert2ogg(self, file_in):
+        tmp_file_out = tempfile.NamedTemporaryFile(
+            prefix='tmp_convert2ogg_file_', dir='/tmp/').name
+        tmp_file_out += '.ogg'
+        rutine = 'ffmpeg -i "%s" -vn -acodec libvorbis -y "%s"' % (
+            file_in, tmp_file_out)
+        args = shlex.split(rutine)
+        self.process = subprocess.Popen(args, stdout=subprocess.PIPE)
+        out, err = self.process.communicate()
+        print(out, err)
+        file_out = get_output_filename(file_in)
+        if os.path.exists(file_out):
+            os.remove(file_out)
+        shutil.copyfile(tmp_file_out, file_out)
+        if os.path.exists(tmp_file_out):
+            os.remove(tmp_file_out)
+
+    def convert2svg(self, file_in):
+        tmp_file_out_1 = tempfile.NamedTemporaryFile(
+            prefix='tmp_2svg_file_step1_',
+            dir='/tmp/').name+'.bmp'
+        tmp_file_out_2 = tempfile.NamedTemporaryFile(
+            prefix='tmp_2svg_file_step2_',
+            dir='/tmp/').name+'.bmp'
+        tmp_file_out_3 = get_output_filename(tmp_file_out_2)
+        rutine2 = 'mkbitmap "%s" -o "%s"' % (tmp_file_out_1, tmp_file_out_2)
+        rutine3 = 'potrace -s "%s"' % (tmp_file_out_2)
+        if os.path.exists((tmp_file_out_1)):
+            os.remove(tmp_file_out_1)
+        if os.path.exists((tmp_file_out_2)):
+            os.remove(tmp_file_out_2)
+        if os.path.exists((tmp_file_out_3)):
+            os.remove(tmp_file_out_3)
+        convertImage2Bmp(file_in, tmp_file_out_1)
+        # ejecuta(rutine1)
+        ejecuta(rutine2)
+        ejecuta(rutine3)
+        file_out = get_output_filename(file_in)
+        if os.path.exists(file_out):
+            os.remove(file_out)
+        shutil.copyfile(tmp_file_out_3, file_out)
+        if os.path.exists((tmp_file_out_1)):
+            os.remove(tmp_file_out_1)
+        if os.path.exists((tmp_file_out_2)):
+            os.remove(tmp_file_out_2)
+        if os.path.exists((tmp_file_out_3)):
+            os.remove(tmp_file_out_3)
+
+    def run(self):
+        total = 0
+        self.emit('started', len(self.elements))
+        try:
+            total = 0
+            for element in self.elements:
+                if self.stopit is True:
+                    self.ok = False
+                    break
+                self.emit('start_one', element)
+                self.convert2svg(element)
+                self.emit('end_one', 1)
+        except Exception as e:
+            self.ok = False
+        try:
+            if self.process is not None:
+                self.process.terminate()
+                self.process = None
+        except Exception as e:
+            print(e)
+        self.emit('ended', self.ok)
 
 
-def get_output_filename(file_in):
-    head, tail = os.path.split(file_in)
-    root, ext = os.path.splitext(tail)
-    file_out = os.path.join(head, root+'.svg')
-    return file_out
+class Progreso(Gtk.Dialog):
+    __gsignals__ = {
+        'i-want-stop': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, ()),
+    }
+
+    def __init__(self, title, parent, max_value):
+        Gtk.Dialog.__init__(self, title, parent)
+        self.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
+        self.set_size_request(330, 30)
+        self.set_resizable(False)
+        self.connect('destroy', self.close)
+        self.set_modal(True)
+        vbox = Gtk.VBox(spacing=5)
+        vbox.set_border_width(5)
+        self.get_content_area().add(vbox)
+        #
+        frame1 = Gtk.Frame()
+        vbox.pack_start(frame1, True, True, 0)
+        table = Gtk.Table(2, 2, False)
+        frame1.add(table)
+        #
+        self.label = Gtk.Label()
+        table.attach(self.label, 0, 2, 0, 1,
+                     xpadding=5,
+                     ypadding=5,
+                     xoptions=Gtk.AttachOptions.SHRINK,
+                     yoptions=Gtk.AttachOptions.EXPAND)
+        #
+        self.progressbar = Gtk.ProgressBar()
+        self.progressbar.set_size_request(300, 0)
+        table.attach(self.progressbar, 0, 1, 1, 2,
+                     xpadding=5,
+                     ypadding=5,
+                     xoptions=Gtk.AttachOptions.SHRINK,
+                     yoptions=Gtk.AttachOptions.EXPAND)
+        button_stop = Gtk.Button()
+        button_stop.set_size_request(40, 40)
+        button_stop.set_image(
+            Gtk.Image.new_from_stock(Gtk.STOCK_STOP, Gtk.IconSize.BUTTON))
+        button_stop.connect('clicked', self.on_button_stop_clicked)
+        table.attach(button_stop, 1, 2, 1, 2,
+                     xpadding=5,
+                     ypadding=5,
+                     xoptions=Gtk.AttachOptions.SHRINK)
+        self.stop = False
+        self.show_all()
+        self.max_value = float(max_value)
+        self.value = 0.0
+
+    def set_max_value(self, anobject, max_value):
+        self.max_value = float(max_value)
+
+    def get_stop(self):
+        return self.stop
+
+    def on_button_stop_clicked(self, widget):
+        self.stop = True
+        self.emit('i-want-stop')
+
+    def close(self, *args):
+        self.destroy()
+
+    def increase(self, anobject, value):
+        self.value += float(value)
+        fraction = self.value/self.max_value
+        self.progressbar.set_fraction(fraction)
+        if self.value >= self.max_value:
+            self.hide()
+
+    def set_element(self, anobject, element):
+        self.label.set_text(_('Converting: %s') % element)
 
 
 def convertImage2Bmp(file_in, file_out):
@@ -68,35 +228,11 @@ def convertImage2Bmp(file_in, file_out):
         img.save(file_out)
 
 
-def convert2svg(file_in):
-    tmp_file_out_1 = tempfile.NamedTemporaryFile(prefix='tmp_2svg_file_step1_',
-                                                 dir='/tmp/').name+'.bmp'
-    tmp_file_out_2 = tempfile.NamedTemporaryFile(prefix='tmp_2svg_file_step2_',
-                                                 dir='/tmp/').name+'.bmp'
-    tmp_file_out_3 = get_output_filename(tmp_file_out_2)
-    # rutine1 = 'convert "%s" "%s"' % (file_in, tmp_file_out_1)
-    rutine2 = 'mkbitmap "%s" -o "%s"' % (tmp_file_out_1, tmp_file_out_2)
-    rutine3 = 'potrace -s "%s"' % (tmp_file_out_2)
-    if os.path.exists((tmp_file_out_1)):
-        os.remove(tmp_file_out_1)
-    if os.path.exists((tmp_file_out_2)):
-        os.remove(tmp_file_out_2)
-    if os.path.exists((tmp_file_out_3)):
-        os.remove(tmp_file_out_3)
-    convertImage2Bmp(file_in, tmp_file_out_1)
-    # ejecuta(rutine1)
-    ejecuta(rutine2)
-    ejecuta(rutine3)
-    file_out = get_output_filename(file_in)
-    if os.path.exists(file_out):
-        os.remove(file_out)
-    shutil.copyfile(tmp_file_out_3, file_out)
-    if os.path.exists((tmp_file_out_1)):
-        os.remove(tmp_file_out_1)
-    if os.path.exists((tmp_file_out_2)):
-        os.remove(tmp_file_out_2)
-    if os.path.exists((tmp_file_out_3)):
-        os.remove(tmp_file_out_3)
+def get_output_filename(file_in):
+    head, tail = os.path.split(file_in)
+    root, ext = os.path.splitext(tail)
+    file_out = os.path.join(head, root+'.svg')
+    return file_out
 
 
 def get_files(files_in):
@@ -107,98 +243,6 @@ def get_files(files_in):
         if os.path.isfile(file_in):
             files.append(file_in)
     return files
-
-
-class Manager(GObject.GObject):
-
-    def __init__(self, files):
-        self.files = files
-
-    def process(self):
-        total = len(self.files)
-        if total > 0:
-            print(self.files)
-            workers = []
-            print('1.- Starting process creating workers')
-            cua = Queue(maxsize=total+1)
-            progreso = Progreso('Converting files...', None, total)
-            total_workers = total if NUM_THREADS > total else NUM_THREADS
-            for i in range(total_workers):
-                worker = Worker(cua)
-                # worker.connect('converted', GLib.idle_add, progreso.increase)
-                # worker.connect('converted', progreso.increase)
-                worker.connect('converted', increase, progreso)
-                worker.start()
-                workers.append(worker)
-            print('2.- Puting task in the queue')
-            for afile in self.files:
-                cua.put(afile)
-            print('3.- Block until all tasks are done')
-            cua.join()
-            print('4.- Stopping workers')
-            for i in range(total_workers):
-                cua.put(None)
-            for worker in workers:
-                worker.join()
-                while Gtk.events_pending():
-                    Gtk.main_iteration()
-            print('5.- The End')
-
-
-class Worker(GObject.GObject, threading.Thread):
-    __gsignals__ = {
-        'converted': (GObject.SIGNAL_RUN_FIRST, GObject.TYPE_NONE, (object, ))
-        }
-
-    def __init__(self, cua):
-        threading.Thread.__init__(self)
-        GObject.GObject.__init__(self)
-        self.setDaemon(True)
-        self.cua = cua
-
-    def run(self):
-        while True:
-            file_in = self.cua.get()
-            if file_in is None:
-                break
-            try:
-                convert2svg(file_in)
-            except Exception as e:
-                print(e)
-            self.emit('converted', file_in)
-            self.cua.task_done()
-
-
-class Progreso(Gtk.Dialog):
-    def __init__(self, title, parent, max_value):
-        #
-        Gtk.Dialog.__init__(self, title, parent)
-        self.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
-        self.set_size_request(330, 40)
-        self.set_resizable(False)
-        self.connect('destroy', self.close)
-        #
-        vbox1 = Gtk.VBox(spacing=5)
-        vbox1.set_border_width(5)
-        self.get_content_area().add(vbox1)
-        #
-        self.progressbar = Gtk.ProgressBar()
-        vbox1.pack_start(self.progressbar, True, True, 0)
-        #
-        self.show_all()
-        #
-        self.max_value = max_value
-        self.value = 0.0
-
-    def close(self, widget=None):
-        self.destroy()
-
-    def increase(self):
-        self.value += 1.0
-        fraction = self.value / self.max_value
-        self.progressbar.set_fraction(fraction)
-        if self.value == self.max_value:
-            self.hide()
 
 
 class SVGConvereterMenuProvider(GObject.GObject, FileManager.MenuProvider):
@@ -216,8 +260,15 @@ class SVGConvereterMenuProvider(GObject.GObject, FileManager.MenuProvider):
 
     def convert(self, menu, selected):
         files = get_files(selected)
-        manager = Manager(files)
-        manager.process()
+        diib = DoItInBackground(files)
+        progreso = Progreso(_('Convert to svg'), None, len(files))
+        diib.connect('started', progreso.set_max_value)
+        diib.connect('start_one', progreso.set_element)
+        diib.connect('end_one', progreso.increase)
+        diib.connect('ended', progreso.close)
+        progreso.connect('i-want-stop', diib.stop)
+        diib.start()
+        progreso.run()
 
     def get_file_items(self, window, sel_items):
         if self.all_files_are_images(sel_items):
